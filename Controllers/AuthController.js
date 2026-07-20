e80cfd7be1ef8633e138bf4ff40af2b4c010e6ea
rajendra-meena <104707658+rajendra-meena@users.noreply.github.com>
2026-07-20 22:06:05 +0530

    Updated

--- Controllers/AuthController.js
+++ Controllers/AuthController.js
@@ -24,7 +24,7 @@ Email: ${user.email}
 Password: ${req.body.password}
 
 Click the link below to log in and start managing your trips:
-https://tmanagment-system.netlify.app/driver/my_trips
+https://transport-managment.netlify.app
 
 Best regards,
 Transport Management System Team
