b5a18e7ab11c2f86fe07a3c820ac0a9e059893e7
raj-meena <raj.meena@hotmail.com>
2026-08-14 00:10:43 +0530

    Initial backend code

--- Backend/Controllers/AuthController.js
+++ Backend/Controllers/AuthController.js
@@ -24,7 +24,7 @@ Email: ${user.email}
 Password: ${req.body.password}
 
 Click the link below to log in and start managing your trips:
-http://localhost:5173
+https://tm-backend-production-35ba.up.railway.app
 
 Best regards,
 Transport Management System Team
